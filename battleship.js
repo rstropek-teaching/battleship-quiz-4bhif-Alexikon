4eed6480083d2f0685c9ae2e6771b6b0c2ec6721
Alex <tischlinger.alex@gmail.com>
2017-10-04 17:31:13 +0200

    working version, but sometimes there are just 3-4 ships - npm install jquery and browser-sync - made index.html to automaticly change to battleship.html

--- battleship.js
+++ battleship.js
@@ -1,12 +1,18 @@
+const MAXCOLUMNS = 10;
+const MAXROWS = 10;
+
+const SHIPTYPES =   [2,    3,        4,    5];
+const SHIPSPAWNS =  [1,    1 + 1,    1,    1];
+
 $(() => {
   // Select table containing the battleground
-  const battleground = $('#battleground');
+  const BATTLEGROUND = $('#battleground');
 
   // Build 10 x 10 grid for battleground
-  for (let row = 0; row < 10; row++) {
+  for (let row = 0; row < MAXROWS; row++) {
     // Create table row
     const tr = $('<tr>');
-    for (let column = 0; column < 10; column++) {
+    for (let column = 0; column < MAXCOLUMNS; column++) {
       // Create table cell with CSS class `water`. Note that we use
       // HTML data attributes  to store the coordinates of each cell
       // (see https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes). 
@@ -18,13 +24,69 @@ $(() => {
     tr.appendTo(battleground);
   }
 
-  $('#generate').click(() => {
-    // Here you have to add your code for building a random battleground.
 
-    // Tip: The next line of code demonstrates how you can select a table cell
-    // using coordinates, remove CSS classes and add CSS classes. 
-    $('td[data-r="1"][data-c="1"]').removeClass('water').addClass('ship');
-    $('td[data-r="2"][data-c="1"]').removeClass('water').addClass('ship');
-    $('td[data-r="3"][data-c="1"]').removeClass('water').addClass('ship');
+
+  $('#generate').click(() => {
+    // reset battlefield
+    for (let row = 0; row < MAXROWS; row++) {
+      for (let column = 0; column < MAXCOLUMNS; column++) {
+        $(`td[data-r="${row}"][data-c="${column}"]`).removeClass('ship').addClass('water');
+      }
+    }
+
+    // create ships
+    for(let shipType = 0; shipType < SHIPTYPES.length; shipType++) {
+      for(let shipSpawn = 0; shipSpawn < SHIPSPAWNS[shipType]; shipSpawn++) {
+        while(!createShip(SHIPTYPES[shipType]));
+      }
+    }
   });
 });
+
+function createShip(shipLength) {
+  let DIRECTION = Math.round(Math.random());
+
+  // Using Truthy ans Falsy Integer with !! & !!!
+  // Also if i reduce the position by the shipLength now, have have not to do extra bound checks
+  // x = column, y = row
+  var posx = Math.round(Math.random() * (10 - ( !!DIRECTION ? shipLength : 0)));
+  var posy = Math.round(Math.random() * (10 - (!!!DIRECTION ? shipLength : 0)));
+
+  // Using Truthy Integer
+  if(!!DIRECTION) {
+    // checking Border on vertical direction/column of ShipLength
+    for(let x = posx; x < posx + shipLength; x++) {
+      if(checkBorder(x, posy)) {
+        return false;
+      }
+    }
+
+    // placing ship
+    for(let x = posx; x < posx + shipLength; x++) {
+      $(`td[data-r="${posy}"][data-c="${x}"]`).removeClass('water').addClass('ship');
+    }
+  } 
+  else {
+    // checking Border on horizontal direction/row of ShipLength
+    for (let y = posy; y < posy + shipLength; y++) {
+      if(checkBorder(posx, y)) {
+        return false;
+      }
+    }
+
+    // placing ship
+    for(let y = posy; y < posy + shipLength; y++) {
+      $(`td[data-r="${y}"][data-c="${posx}"]`).removeClass('water').addClass('ship');
+    }
+  }
+
+  return true;
+}
+
+// checks Border   if a ship class is on border --> true
+function checkBorder(posx, posy) {
+  return ((posx + 1 <= 9 && $(`td[data-r="${posy}"][data-c="${posx + 1}"]`).hasClass('ship')) ||
+          (posx - 1 >= 0 && $(`td[data-r="${posy}"][data-c="${posx - 1}"]`).hasClass('ship')) ||
+          (posy + 1 <= 9 && $(`td[data-r="${posy + 1}"][data-c="${posx}"]`).hasClass('ship')) ||
+          (posy - 1 >= 0 && $(`td[data-r="${posy - 1}"][data-c="${posx}"]`).hasClass('ship')));
+}
